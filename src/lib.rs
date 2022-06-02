407383998dc27f60903551098c1076c471a3cd33
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2022-06-02 22:01:47 +0200

    whatever

---
+++ src/lib.rs
@@ -0,0 +1,113 @@
+//! This contract implements simple counter backed by storage on blockchain.
+//!
+//! The contract provides methods to [increment] / [decrement] counter and
+//! [get it's current value][get_num] or [reset].
+//!
+//! [increment]: struct.Counter.html#method.increment
+//! [decrement]: struct.Counter.html#method.decrement
+//! [get_num]: struct.Counter.html#method.get_num
+//! [reset]: struct.Counter.html#method.reset
+
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen};
+
+near_sdk::setup_alloc!();
+
+// add the following attributes to prepare your code for serialization and invocation on the blockchain
+// More built-in Rust attributes here: https://doc.rust-lang.org/reference/attributes.html#built-in-attributes-index
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Counter {}
+
+#[near_bindgen]
+impl Counter {
+    pub fn verify(&self, signature_p1: [u8; 32], signature_p2: [u8; 32], msg: [u8; 32]) -> bool {
+        let private_key: &[u8] = &[
+            1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103,
+            137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239,
+        ];
+        let public_key: &[u8] = &[
+            32, 122, 6, 120, 146, 130, 30, 37, 215, 112, 241, 251, 160, 196, 124, 17, 255, 75, 129,
+            62, 84, 22, 46, 206, 158, 184, 57, 224, 118, 35, 26, 182,
+        ];
+        let kp = ed25519_dalek::Keypair::from_bytes([private_key, public_key].concat().as_ref())
+            .unwrap();
+        let signature =
+            &ed25519_dalek::Signature::from_bytes(&[signature_p1, signature_p2].concat()).unwrap();
+        kp.verify(&msg, signature).unwrap();
+        env::log("Make sure you don't overflow, my friend.".as_bytes());
+        true
+    }
+}
+
+/*
+ * the rest of this file sets up unit tests
+ * to run these, the command will be:
+ * cargo test --package rust-counter-tutorial -- --nocapture
+ * Note: 'rust-counter-tutorial' comes from cargo.toml's 'name' key
+ */
+
+// use the attribute below for unit tests
+#[cfg(test)]
+mod tests {
+    use std::convert::TryInto;
+
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+
+    // part of writing unit tests is setting up a mock context
+    // in this example, this is only needed for env::log in the contract
+    // this is also a useful list to peek at when wondering what's available in env::*
+    fn get_context(input: Vec<u8>, is_view: bool) -> VMContext {
+        VMContext {
+            current_account_id: "alice.testnet".to_string(),
+            signer_account_id: "robert.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id: "jane.testnet".to_string(),
+            input,
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage: 0,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+
+    // mark individual unit tests with #[test] for them to be registered and fired
+    #[test]
+    fn increment() {
+        // set up the mock context into the testing environment
+        let context = get_context(vec![], false);
+        testing_env!(context);
+        let contract = Counter {};
+
+        // TO GENERATE DATA: https://paulmillr.com/noble/
+
+        let signature: [u8; 64] = [
+            145, 193, 203, 18, 114, 227, 14, 117, 33, 213, 121, 66, 130, 14, 25, 4, 36, 120, 46,
+            142, 226, 215, 7, 66, 122, 112, 97, 30, 249, 135, 61, 165, 221, 249, 252, 23, 105, 40,
+            56, 70, 31, 152, 236, 141, 154, 122, 207, 20, 75, 118, 79, 90, 168, 6, 221, 122, 213,
+            29, 126, 196, 216, 104, 191, 6,
+        ];
+
+        // 32 bytes message
+        let message: [u8; 32] = [
+            107, 97, 106, 100, 108, 102, 107, 106, 97, 108, 107, 102, 106, 97, 107, 108, 102, 106,
+            100, 107, 108, 97, 100, 106, 102, 107, 108, 106, 97, 100, 115, 107,
+        ];
+
+        // let signature = ed25519_dalek::Signature::from_bytes(signature).unwrap();
+        contract.verify(
+            signature[..32].try_into().unwrap(),
+            signature[32..].try_into().unwrap(),
+            message,
+        );
+    }
+}
